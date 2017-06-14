a852f94167d70a213c7629fdac58b3ceb152fa34
Jonathan Simon <jbsimon@google.com>
2017-06-14 06:39:23 -0700

    Add trace sample

---
+++ trace/Trace/Controllers/TraceController.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Diagnostics.AspNet;
+using Google.Cloud.Diagnostics.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Trace.Controllers
+{
+    public class TraceController : Controller
+    {
+        // This incoming HTTP request should be captured by Trace.
+        public ActionResult Index()
+        {
+            IManagedTracer tracer = CloudTrace.Tracer;
+            using (tracer.StartSpan(nameof(Index)))
+            {
+                var traceHeaderHandler = new TraceHeaderPropagatingHandler(() => tracer);
+                var response = TraceOutgoing(traceHeaderHandler);
+                ViewData["text"] = response.Result.ToString();
+                return View();
+            }
+        }
+
+        // This outgoing HTTP request should be captured by Trace.
+        public async Task<string> TraceOutgoing(TraceHeaderPropagatingHandler traceHeaderHandler)
+        {
+            // Add a handler to trace outgoing requests and to propagate the trace header.
+            using (var httpClient = new HttpClient(traceHeaderHandler))
+            {
+                string url = "https://www.googleapis.com/discovery/v1/apis";
+                using (var response = await httpClient.GetAsync(url).ConfigureAwait(false))
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+    }
+}
---
+++ trace/Trace/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Diagnostics.AspNet;
+using Google.Cloud.Diagnostics.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Trace
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+
+        public override void Init()
+        {
+            base.Init();
+            TraceConfiguration traceConfig = TraceConfiguration.Create(bufferOptions: BufferOptions.NoBuffer());
+            CloudTrace.Initialize(this, "your-project-id", traceConfig);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
