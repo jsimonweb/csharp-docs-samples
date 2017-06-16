8c16fd1ae7e91e8448bc9d42aeee5390b1d68cc0
Jonathan Simon <jbsimon@google.com>
2017-06-16 14:10:41 -0700

    add Trace for .NET framework

---
+++ trace/api/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Trace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string result = "Trace created.";
+            //return Ok(result);
+            return View();
+        }
+    }
+}
---
+++ trace/api/Controllers/TraceController.cs
@@ -0,0 +1,48 @@
+﻿using Google.Cloud.Diagnostics.AspNet;
+using Google.Cloud.Diagnostics.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Trace.Controllers
+{
+    // [START trace_example]
+    public class TraceController : Controller
+    {
+        // This incoming HTTP request should be captured by Trace.
+        public ActionResult Index()
+        {
+            using (CloudTrace.Tracer.StartSpan(nameof(Index)))
+            {
+                string url = "https://www.googleapis.com/discovery/v1/apis";
+                var response = TraceOutgoing(url);
+                ViewData["text"] = response.Result.ToString();
+                return View();
+            }
+        }
+
+        public async Task<string> TraceOutgoing(string url)
+        {
+            // Manually trace a specific operation.
+            using (CloudTrace.Tracer.StartSpan("get-api-discovery-doc"))
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    // This outgoing HTTP request should be captured by Trace.
+                    using (var response = await httpClient.GetAsync(url)
+                        .ConfigureAwait(false))
+                    {
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+        }
+    }
+    // [END trace_example]
+}
